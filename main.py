071020f0462adea736dda301068d0c4744b22957
Szymon Hudziak <szymonh.dg@gmail.com>
2022-09-07 18:52:06 +0200

    add i2c communication

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from machine import Pin,UART
+from machine import Pin, UART, I2C
+from pca import PCA9685
+from servo import Servos
 
-uart = UART(0,9600)
+uart = UART(0,9600) #Initialize UART
+
+sda = Pin(2) #SDA pin
+scl = Pin(3) #SCL pin
+i2c_id = 1 #I2C id
+
+i2c = I2C(id=i2c_id, sda=sda, scl=scl) #Initialize I2C
+pca = PCA9685(i2c=i2c) #Create PCA9685 object
+servos = Servos(i2c=i2c) #Create Servos object
+
+print(i2c.scan())
 
 while True:
     if uart.any():
