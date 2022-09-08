58e719c0138fb3768752b3165b4a0f02e41e9762
Szymon Hudziak <szymonh.dg@gmail.com>
2022-09-08 15:16:30 +0200

    add functions to control servos

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from machine import Pin, UART, I2C
+from time import sleep
 from pca import PCA9685
 from servo import Servos
+from braille_dictionary import braille_dictionary as b_dict
 
 uart = UART(0,9600) #Initialize UART
 
@@ -14,18 +16,52 @@ servos = Servos(i2c=i2c) #Create Servos object
 
 
 braille_dots = [0, 0, 0, 0, 0, 0]
+serv_0_position = [90, 90, 90, 90, 90, 90]
+serv_1_position = [150, 150, 150, 30, 30, 30]
+serv_current_position = [90, 90, 90, 90, 90, 90]
+serv_position = [90, 90, 90, 90, 90, 90]
 
 
 def letter_to_braille(letter):
-    return braille_dict.get(letter, "nothing")
+    braille_data = b_dict.get(letter, [0, 0, 0, 0, 0, 0])
+    return braille_data
 
+def physical_representation(letter_data):
+    for i in letter_data:
+        if letter_data[i] == 1:
+            serv_position[i] = serv_1_position[i]
+        else:
+            serv_position[i] = serv_0_position[i]
+
+def update_servos():
+    for i in range(6):
+        if serv_current_position[i] < serv_position[i]:
+            for j in range(serv_current_position[i], serv_position[i], 1):
+                servos.position(index=i, degrees=j)
+                serv_current_position[i] = j
+                sleep(0.01)
+        else:
+            for j in range(serv_current_position[i], serv_position[i], -1):
+                servos.position(index=i, degrees=j)
+                serv_current_position[i] = j
+                sleep(0.01)
+                
+def reset_servos():
+    for i in range(6):
+        for i in range(6):
+            servos.position(index=i, degrees=serv_0_position[i]) 
+
+reset_servos()
 while True:
     if uart.any():
         command = uart.read()
         message = str(command.rstrip(), 'utf8')#str(command)#command.decode('utf-8')#str(command.rstrip(), 'utf8')
         print(message)
-        
-        print(letter_to_braille(message))
+        braille_dots = letter_to_braille(message)     
+#         print(letter_to_braille(message))
+        print(braille_dots)
+        physical_representation(braille_dots)
+        update_servos()
         
 #         if message == "180":
 #             servos.position(index=0, degrees=180)
@@ -34,5 +70,7 @@ while True:
 #         else:
 #             servos.position(index=0, degrees=0)
 
-        
+# def update_servos():
+#     for i in range(6):
+#         servos.position(index=i, degrees=serv_position[i])        
 
