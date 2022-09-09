14f486434df225193acd133438e85cb4ce63908c
Szymon Hudziak <szymonh.dg@gmail.com>
2022-09-09 15:39:10 +0200

    add buttons and interrupts

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ i2c_sda = Pin(2) #SDA pin
 i2c_scl = Pin(3) #SCL pin
 i2c_id = 1 #I2C id
 
+button_increase_pin = 15
+button_decrease_pin = 14
+current_letter = 0
+last_time_increase = 0
+last_time_decrease = 0
+
 i2c = I2C(id=i2c_id, sda=i2c_sda, scl=i2c_scl) #Initialize I2C
 pca = PCA9685(i2c=i2c) #Create PCA9685 object
 servos = Servos(i2c=i2c) #Create Servos object
@@ -49,17 +55,40 @@ def update_servos():
 def reset_servos():
     for i in range(6):
         for i in range(6):
-            servos.position(index=i, degrees=serv_0_position[i]) 
+            servos.position(index=i, degrees=serv_0_position[i])
+            
+def handle_interrupt_increase(Pin):
+    global current_letter, last_time_increase
+    new_time = ticks_ms()
+    if (new_time - last_time_increase) > 200:
+        counter = counter + 1
+        print(counter)
+        last_time_increase = new_time
+        
+def handle_interrupt_decrease(Pin):
+    global current_letter, last_time_decrease
+    new_time = ticks_ms()
+    if (new_time - last_time_decrease) > 200 and counter > 0:
+        counter = counter - 1
+        print(counter)
+        last_time_decrease = new_time
+        
+button_increase = Pin(button_increase_pin, Pin.IN, Pin.PULL_UP)
+button_decrease = Pin(button_decrease_pin, Pin.IN, Pin.PULL_UP)
+
+button_increase.irq(trigger=Pin.IRQ_FALLING, handler=handle_interrupt_increase)
+button_decrease.irq(trigger=Pin.IRQ_FALLING, handler=handle_interrupt_decrease)
+
 
 reset_servos()
 while True:
     if uart.any():
         command = uart.read()
         message = str(command.rstrip(), 'utf8')#str(command)#command.decode('utf-8')#str(command.rstrip(), 'utf8')
-        print(message)
+        print(message) #test
         braille_dots = letter_to_braille(message)     
 #         print(letter_to_braille(message))
-        print(braille_dots)
+        print(braille_dots) #test
         physical_representation(braille_dots)
         update_servos()
         
